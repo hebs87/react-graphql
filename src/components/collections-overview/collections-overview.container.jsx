cfc266886de9157b69429cc190b649603c1edd34
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-04 20:11:07 +0000

    Removed Redux code in CollectionsOverview component and fetched collections data using GraphQL instead

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+// Gets our query, gets info from GraphQL and then
+// gives it back to us as a function that we can
+// use in the relevant component, which has access
+// to the data we want
+import { Query } from 'react-apollo';
+// Allows us to make GraphQL requests
+import { gql } from 'apollo-boost';
+
+import CollectionsOverview from "./collections-overview.component";
+import Spinner from "../spinner/spinner.component";
+
+// The query to our database - the const name is in all CAPS
+// as it never changes
+const GET_COLLECTIONS = gql`
+    {
+        collections {
+            id
+            title
+            items {
+                id
+                name
+                price
+                imageUrl
+            }
+        }
+    }
+`;
+
+// The CollectionsOverviewContainer will be a functional component
+// that returns our Query component that wraps around the
+// CollectionsOverview component. The Query component take a
+// query property equal to our previously defined query
+const CollectionsOverviewContainer = () => (
+    // The Query gives us back a function. On that function, we
+    // will get an object that holds a lot of different props.
+    // The main ones we need are "loading", "error" and "data".
+    // We can get the error if we have any error handling
+    // components, but we don't in our app so we don't need it
+    <Query query={GET_COLLECTIONS}>
+        {
+            ({loading, error, data}) => {
+                // If loading is true, we render the Spinner
+                if (loading) return <Spinner />;
+                // If loading is false, we render the CollectionsOverview
+                // and pass in the data.collections as collections prop
+                return <CollectionsOverview collections={data.collections} />;
+            }
+        }
+    </Query>
+);
+
+export default CollectionsOverviewContainer;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+// When we make default imports, we have access in the
+// curly braces to the default alias. So we are essentially
+// saying we want to import whatever is exported by default
+// (CollectionsOverviewContainer) in the relevant file, but
+// we want to call it CollectionsOverview instead
+// Doing this saves us having to change any other code in
+// our app
+import {
+    default as CollectionsOverview
+} from '../../components/collections-overview/collections-overview.container';
 import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({ match }) => (
