b395abaf50baef5b0ed1d32daafba6726a5feb27
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-04 19:20:00 +0000

    Removed test Apollo/GraphQL code from index.js as it is no longer needed

--- src/index.js
+++ src/index.js
@@ -12,8 +12,7 @@ import {createHttpLink} from 'apollo-link-http';
 // Allows Apollo to cache the fetched data
 import {InMemoryCache} from 'apollo-cache-inmemory';
 // The Client itself
-// gql allows us to test the client - can be removed once tested
-import {ApolloClient, gql} from 'apollo-boost';
+import {ApolloClient} from 'apollo-boost';
 
 import {store, persistor} from './redux/store';
 
@@ -32,36 +31,11 @@ const httpLink = createHttpLink({
 const cache = new InMemoryCache();
 
 // Create the client, which takes an object in which we set
-// the link to thee httpLink and the cache to the cache
+// the link to the httpLink and the cache to the cache
 const client = new ApolloClient({
     link: httpLink,
     cache
 });
-
-// Test the Client using a query. In order to use syntax
-// similar to GraphQL, we call the gql method and then
-// specify the query within the back ticks. This returns
-// us a promise which resolved to the data, if it manages
-// to fetch the data successfully
-// We then console log the response
-// We can remove this once we've successfully tested the
-// connection
-client.query({
-    query: gql`
-        {
-            getCollectionsByTitle(title: "hats") {
-                id
-                title
-                items {
-                    id
-                    name
-                    price
-                    imageUrl
-                }
-            }
-        }
-    `
-}).then(res => console.log(res));
 
 ReactDOM.render(
     // We wrap the app in the ApolloProvider component and
